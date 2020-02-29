388972716720717d0c32890969232c754f79eeab
ag1910 <58875503+ag1910@users.noreply.github.com>
2020-02-29 11:44:47 +0300

    oyun eklendi

---
+++ oyun.js
@@ -0,0 +1,12 @@
+onload="open.html"
+onload="game2.html"
+
+alert("1.oyuncunun sırası")
+function myFunction() {
+	onload="game3.css"
+var x = document.getElementById("button");
+if (x.style.display === "none") {
+x.style.display = "block";
+} else {
+x.style.display = "none";
+}
